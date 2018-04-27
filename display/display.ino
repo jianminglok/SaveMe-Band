308fb66c73948482bbec93d32ab18296e15bd816
Reloin <yihengleong@gmail.com>
2018-04-27 23:58:43 +0800

    display showing something

---
+++ display/display.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <Wire.h>
+//#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+
+void setup(){
+
+  Serial.begin(115200);
+
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (
+
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.setCursor(0,0);
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.println("SaveMe Band");
+  display.display();//to show uploaded text
+
+}
+
+void loop(){
+
+
+}
