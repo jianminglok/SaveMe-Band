7323dba75940723de57088820b7de2dfe3ed2426
Reloin <yihengleong@gmail.com>
2018-04-28 00:30:12 +0800

    splash screen

--- display/display.ino
+++ display/display.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_SSD1306.h>
+#include <min.h>
 
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
@@ -12,6 +12,9 @@ void setup(){
   Serial.begin(115200);
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (
+    display.display();
+    delay(3000);
+    display.clearDisplay();
 
   // Show image buffer on the display hardware.
   // Since the buffer is intialized with an Adafruit splashscreen
