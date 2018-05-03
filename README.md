375f97a756c3fb8d035b6a1045877efadc9edf5a
Jian Ming Lok <jianminglok@hotmail.com>
2018-05-03 17:15:45 +0800

    Various fixes and README update

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # SaveMe-Band
 SINDEX 2018 Project
 
-Libraries required:
-TinyGSM
+##Libraries required:
+**TinyGSM**
 https://github.com/vshymanskyy/TinyGSM
-refer to link to download the librabires
 
-for display
+**StreamDebugger**
+https://github.com/vshymanskyy/StreamDebugger
+
+**Time**
+https://github.com/PaulStoffregen/Time
+
+##Display branch
 I2C location: 0x3C
+
 SSD1306
 https://github.com/adafruit/Adafruit_SSD1306
+
 GFX graphics
 https://github.com/adafruit/Adafruit-GFX-Library
 
+Refer to links above to download the libraries
--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,15 @@
-#define TINY_GSM_MODEM_SIM868;
-#define SMS_TARGET  "+60132699212" //target receiver
+#define TINY_GSM_MODEM_SIM868
+#define SMS_TARGET  "+60109829252" //target receiver
 
 #include <TinyGsmClient.h>
 #include <SoftwareSerial.h>
-//#include <CurieTime.h>
+#include <TimeLib.h>
+
+#define SerialMon Serial
 
 SoftwareSerial SerialAT(2, 3); // RX, TX for sim868
 
-//#define DUMP_AT_COMMANDS
+#define DUMP_AT_COMMANDS
 #define TINY_GSM_DEBUG SerialMon
 
 #ifdef DUMP_AT_COMMANDS
@@ -18,9 +20,7 @@ TinyGsm modem(debugger);
 TinyGsm modem(SerialAT);
 #endif
 
-int btn = 4;
-int ledPin = 13;
-int ledState = LOW;
+int btn = 8;
 int val = 0;
 
 int buttonState;
@@ -31,15 +31,18 @@ String imei;
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;
 
+float gps_latitude;
+float gps_longitude;
+float gps_speed;
+int gps_altitude;
+int gps_view_satellites;
+int gps_used_satellites;
+
 void setup() {
 
   pinMode(btn, INPUT);
-  pinMode(ledPin, OUTPUT);
 
-  // set initial LED state
-  digitalWrite(ledPin, ledState);
-  
-  Serial.begin(115200);
+  SerialMon.begin(115200);
 
   delay(10);
 
@@ -52,52 +55,46 @@ void setup() {
 
   // Restart takes quite some time
   // To skip it, call init() instead of restart()
-  Serial.println(F("Initializing modem..."));
+  DBG("Initializing modem...");
   modem.init();
 
   String modemInfo = modem.getModemInfo();
-  Serial.print(F("Modem: "));
-  Serial.println(modemInfo);
+  DBG("Modem:", modemInfo);
 
   imei = modem.getIMEI();
-  Serial.print("IMEI:" + imei);
+  DBG("IMEI:", imei);
+
+  modem.enableGPS();
 
 }
 
 void loop() {
 
-  modem.enableGPS();
-  float gps_latitude;
-  float gps_longitude;
-  float gps_speed;
-  int gps_altitude;
-  int gps_view_satellites;
-  int gps_used_satellites;
   bool gps_fixstatus = modem.getGPS(&gps_latitude, &gps_longitude, &gps_speed, &gps_altitude, &gps_view_satellites, &gps_used_satellites);
   if ( gps_fixstatus ) {
     
-    Serial.print(F("#GPS Location: LAT: "));
-    Serial.println(gps_latitude);
-    Serial.print(F(" LONG: "));
-    Serial.println(gps_longitude);
-    Serial.print(F(" SPEED: "));
-    Serial.println(gps_speed);
-    Serial.print(F(" ALTITUDE: "));
-    Serial.println(gps_altitude);
-    Serial.print(F(" USED SATELITES: "));
-    Serial.println(gps_view_satellites);
-    Serial.print(F(" VIEWED STELITES: "));
-    Serial.println(gps_used_satellites);
+    debugger.print(F("#GPS Location: LAT: "));
+    debugger.println(gps_latitude);
+    debugger.print(F(" LONG: "));
+    debugger.println(gps_longitude);
+    debugger.print(F(" SPEED: "));
+    debugger.println(gps_speed);
+    debugger.print(F(" ALTITUDE: "));
+    debugger.println(gps_altitude);
+    debugger.print(F(" USED SATELITES: "));
+    debugger.println(gps_view_satellites);
+    debugger.print(F(" VIEWED STELITES: "));
+    debugger.println(gps_used_satellites);
   
     int gps_year, gps_month, gps_day, gps_hour, gps_minute, gps_second;
     if ( modem.getGPSTime(&gps_year, &gps_month, &gps_day, &gps_hour, &gps_minute, &gps_second) ) {
       //Sync time if it's diffrent.
-      //setTime(gps_hour, gps_minute, gps_second, gps_day, gps_month, gps_year);
+      setTime(gps_hour, gps_minute, gps_second, gps_day, gps_month, gps_year);
       //set_time(gps_year, gps_month, gps_day, gps_hour, gps_minute, gps_second);
     }
     
-}
-  
+  }
+
   // put your main code here, to run repeatedly:
   val = digitalRead(btn);  // read input value
 
@@ -118,19 +115,15 @@ void loop() {
     // if the button state has changed:
     // only toggle the LED if the new button state is HIGH
       if (val == HIGH) {
-        // turn LED on:
-        Serial.println("Button is not pushed");  // turn LED OFF
-        ledState = !ledState;
+        debugger.println("Button is not pushed");  // turn LED OFF
       } else {
         // turn LED off:
-        Serial.println("Button is pushed");
-        ledState = !ledState;
+        debugger.println("Button is pushed");
         sendSMS();  // turn LED ON
       }
     }
   }
 
-  digitalWrite(ledPin, ledState);
   lastButtonState = val;
 }
 
